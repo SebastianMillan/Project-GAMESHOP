9a76f2476d02ed56b0bd64bfa57d8574d2cabb62
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-05-03 17:20:18 +0200

    CASA - Crear POJO videojuego con sus enum junto a empezar a colocar thymeleaf en el formulario del mismo

---
+++ src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/model/VideojuegoGenero.java
@@ -0,0 +1,5 @@
+package com.salesianostriana.dam.videogamesshopproject_sebastianmillan.model;
+
+public enum VideojuegoGenero {
+	ARCADE, PLATAFORMAS, DISPAROS, AVENTURA, RPG, ACCIÓN, PUZZLE, ESTRATEGIA, SIMULACIÓN, DEPORTES, CARRERRAS;
+}
