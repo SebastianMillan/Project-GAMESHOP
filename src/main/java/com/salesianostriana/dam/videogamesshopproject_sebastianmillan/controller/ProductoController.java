65e06b178c356b4a00d2fa8dba8b08a2a7805d3b
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-05-05 11:58:40 +0200

    INSTITUTO - Crear los servicios y los controllers

--- src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
@@ -18,20 +18,5 @@ public class ProductoController {
 		//model.addAttribute("listaProductos", productoService.findAll());
 		return "index";	
 	}
-	@GetMapping("/formCliente")
-	public String lista(Model model) {
-		//model.addAttribute("listaProductos", productoService.findAll());
-		return "index";	
-	}
 
-	@GetMapping("/productoDetalle")
-	public String detalle(Model model) {
-		//model.addAttribute("listaProductos", productoService.findAll());
-		return "producto_detalle";	
-	}
-	@GetMapping("/consolaAdmin")
-	public String admin(Model model) {
-		//model.addAttribute("listaProductos", productoService.findAll());
-		return "admin_consola";	
-	}
 }
