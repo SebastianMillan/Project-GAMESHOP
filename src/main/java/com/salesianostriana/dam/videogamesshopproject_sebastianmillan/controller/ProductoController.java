03d55b8eb190a8ef368c61a46a4027ea97cfb72a
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-05-10 18:51:58 +0200

    CASA - Añadir videojuego, falta pequeña corrección de ajustes

--- src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
@@ -1,17 +1,36 @@
 package com.salesianostriana.dam.videogamesshopproject_sebastianmillan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.salesianostriana.dam.videogamesshopproject_sebastianmillan.model.Producto;
+import com.salesianostriana.dam.videogamesshopproject_sebastianmillan.service.ProductoService;
 
 
 @Controller
 public class ProductoController {
 	
-	@GetMapping("/formVideojuego")
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("/addVideojuego")
 	public String showFormVideojuego(Model model) {
-		//Producto videojuego = new Producto();
+		model.addAttribute("producto", new Producto());
+		model.addAttribute("pegis", productoService.obtenerValoresPegi());
+		model.addAttribute("generos", productoService.obtenerValoresGenero());
+		model.addAttribute("tipos", productoService.obtenerValoresTipoDesarrollo());
+
 		return "form_videojuego";	
+	}
+	
+	@PostMapping("/addVideojuego/submit")
+	public String processFormVideojuego(@ModelAttribute("producto") Producto p) {
+		productoService.save(p);
+		return "redirect:/admin/";
 	}
 
 
