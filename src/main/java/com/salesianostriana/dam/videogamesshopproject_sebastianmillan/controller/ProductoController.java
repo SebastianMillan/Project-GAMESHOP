a659c3357f06c9631116c0bac5bd7b810a7065ab
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-05-10 19:18:30 +0200

    CASA - Borrado y editado de objetos, falta por corregir los enum

--- src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/videogamesshopproject_sebastianmillan/controller/ProductoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.videogamesshopproject_sebastianmillan.model.Producto;
@@ -18,7 +19,7 @@ public class ProductoController {
 	private ProductoService productoService;
 	
 	@GetMapping("/addVideojuego")
-	public String showFormVideojuego(Model model) {
+	public String showFormAddVideojuego(Model model) {
 		model.addAttribute("producto", new Producto());
 		model.addAttribute("pegis", productoService.obtenerValoresPegi());
 		model.addAttribute("generos", productoService.obtenerValoresGenero());
@@ -28,8 +29,31 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/addVideojuego/submit")
-	public String processFormVideojuego(@ModelAttribute("producto") Producto p) {
+	public String processFormAddVideojuego(@ModelAttribute("producto") Producto p) {
 		productoService.save(p);
+		return "redirect:/admin/";
+	}
+	
+	@GetMapping("/editVideojuego/{id}")
+	public String showFormEditVideojuego(@PathVariable("id") long id, Model model) {
+		Producto pEditar = productoService.findById(id).get();
+		if(pEditar!=null) {
+			model.addAttribute("producto", pEditar);
+			return "form_videojuego";
+		} else {
+			return "redirect:/admin/";
+		}
+		
+	}
+	@PostMapping("/editVideojuego/submit")
+	public String processFormEditVideojuego(@ModelAttribute("producto") Producto p) {
+		productoService.save(p);
+		return "redirect:/admin/";
+	}
+	
+	@GetMapping("/deleteVideojuego/{id}")
+	public String deleteVideojuego(@PathVariable("id") long id) {
+		productoService.deleteById(id);
 		return "redirect:/admin/";
 	}
 
